28470f60fbf7c77ff02d0693f7581b46edc0e11e
cameron <cameron.murtagh00@gmail.com>
2025-12-06 11:21:23 +1100

    small cleanup and add license

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-clang++ -Wall -Wextra -Wpedantic --std=c++23 --target=wasm32 -O3 -flto -nostdlib -c --include-directory=include -o listgen.o listgen.cpp
-clang++ -Wall -Wextra -Wpedantic --std=c++23 --target=wasm32 -O3 -flto -nostdlib -c --include-directory=include -o string.o string.cpp
+clang++ -Wall -Wextra -Wpedantic --std=c++23 --target=wasm32 -O3 -flto -nostdlib -c --include-directory=include -o listgen.o src/listgen.cpp
+clang++ -Wall -Wextra -Wpedantic --std=c++23 --target=wasm32 -O3 -flto -nostdlib -c --include-directory=include -o string.o src/string.cpp
 wasm-ld --no-entry --lto-O3 -o listgen.wasm listgen.o string.o
