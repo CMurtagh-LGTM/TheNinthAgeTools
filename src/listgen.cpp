2152c106ac7d55c5d10ce410696d0596cbc058f8
cameron <cameron.murtagh00@gmail.com>
2026-01-02 15:22:44 +1100

    some initial work on the table

--- src/listgen.cpp
+++ src/listgen.cpp
@@ -4,35 +4,61 @@
 
 #include <cstring>
 #include <string>
+#include <format>
 #include <toml++/toml.hpp>
 
-int main() {
+struct entry_t {
+  std::string_view name;
+  std::string_view key;
+  bool inches;
+};
 
+constexpr std::array<const entry_t, 3> entry_descriptions = {{
+  {"Cha", "cha", true},
+  {"Mob", "mob", true},
+  {"Dis", "dis", false},
+}};
+
+void add_table(const toml::table& book, const std::string_view name) {
   auto content = dom::get_by_id("content");
-  auto img = dom::append_img_child(content);
-  dom::set_img_src(img, "./pics/logo_character.png");
+  auto book_entry = book[name];
 
   auto table = dom::append_table_child(content);
-  auto tr = dom::append_tr_child(table);
-  auto td = dom::append_td_child(tr);
-  dom::set_text(td, "cool");
+  auto global_header = dom::append_tr_child(table);
+  auto global_data = dom::append_tr_child(table);
+
+  for (const auto& entry_description: entry_descriptions) {
+    auto header = dom::append_td_child(global_header);
+    dom::set_text(header, entry_description.name);
+    auto data = dom::append_td_child(global_data);
+    int value = book_entry[entry_description.key].value_or(0);
+    if (entry_description.inches) {
+      dom::set_text(data, std::format("{}\"", value));
+    } else {
+      dom::set_text(data, std::format("{}", value));
+    }
+  }
+}
+
+
+int main() {
+  // auto content = dom::get_by_id("content");
+  // auto img = dom::append_img_child(content);
+  // dom::set_img_src(img, "./pics/logo_character.png");
+
 
   using namespace std::string_view_literals;
   static constexpr auto source = R"(
-      str = "hello world"
-
-      numbers = [ 1, 2, 3, "four", 5.0 ]
-      vegetables = [ "tomato", "onion", "mushroom", "lettuce" ]
-      minerals = [ "quartz", "iron", "copper", "diamond" ]
-
-      [animals]
-      cats = [ "tiger", "lion", "puma" ]
-      birds = [ "macaw", "pigeon", "canary" ]
-      fish = [ "salmon", "trout", "carp" ]
+    [marshal]
+    cha = 4
+    mob = 4
+    dis = 8
   )"sv;
-  auto tbl = toml::parse(source);
-
-  std::string_view str = tbl["str"].value_or(""sv);
-  wasm::log(str);
+  auto book = toml::parse(source);
+  if (!book) {
+    wasm::log("failed to parse toml book");
+    return 0;
+  }
+  add_table(book, "marshal");
 }
 
