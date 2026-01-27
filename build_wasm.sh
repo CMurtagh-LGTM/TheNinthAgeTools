2b768ae298caa73e6e889a6a3aa619512ef2139e
cameron <cameron.murtagh00@gmail.com>
2026-01-27 19:10:11 +1100

    clean up build script a little

--- build_wasm.sh
+++ build_wasm.sh
@@ -4,6 +4,4 @@ cmake -B build_wasm -G Ninja -DCMAKE_TOOLCHAIN_FILE=../wasi-sdk/share/cmake/wasi
 cmake --build build_wasm
 
 # ln -s $PWD/build_wasm/compile_commands.json compile_commands.json
-# wasm-opt build_wasm/listgen.wasm --enable-bulk-memory-opt --all-features -O3 -Oz -o build_wasm/listgen_opt.wasm
-wasm2wat build_wasm/listgen.wasm > build_wasm/listgen.wat
-# wasm2wat build_wasm/listgen_opt.wasm > build_wasm/listgen_opt.wat
+# wasm2wat build_wasm/listgen.wasm > build_wasm/listgen.wat
