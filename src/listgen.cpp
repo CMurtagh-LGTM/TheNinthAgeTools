566fad549441b5669e5b6ef9ca2577bc2685e9e8
cameron <cameron.murtagh00@gmail.com>
2026-01-31 15:58:39 +1100

    add state infantry data

--- src/listgen.cpp
+++ src/listgen.cpp
@@ -38,10 +38,37 @@ int main() {
     ap = 1
     agi = 1
     offensive_rules = ["Human", "Metal Armour"]
+
+    ["State Infantry"]
+    points = 160
+    model_count = 20
+    rarity = "Regular"
+    base = "20x20"
+    height = 1
+    cha = 4
+    mob = 4
+    dis = 7
+    model_rules = ["Parent Unit"]
+    hp = 1
+    def = 3
+    res = 3
+    arm = 2
+    [["State Infantry".offensive]]
+    name = "State Infantry"
+    att = 1
+    off = 3
+    str = 3
+    ap = 0
+    agi = 3
+    offensive_rules = ["Human", "Metal Armour"]
+
   )"sv;
   static constexpr auto model_rules_source = R"(
     Disciplined = "Command Tests taken by a unit containing Disciplined gain {{Minimized}}."
     Minimized = "For each instance of Minimised, add a dice to the roll, and discard the dice that rolled the highest result."
+    "Parent Unit" = "A unit containing a model with this rule is referred to as a ‘Parent Unit’. Parent Unit gains {{Fight in Extra Rank}} while there is at least one {{Support Unit}} engaged in the same combat."
+    "Support Unit" = "-"
+    "Fight in Extra Rank" = "A model part with Fight in Extra Rank, or using a weapon with Fight in Extra Rank, can make Supporting Attacks from one additional rank."
   )"sv;
   auto book = toml::parse(source);
   auto model_rules = toml::parse(model_rules_source);
@@ -49,6 +76,6 @@ int main() {
     wasm::log("failed to parse toml book");
     return 0;
   }
-  Unit unit = table::add_unit(model_rules, book, "Marshal");
-  std::get<reactive::Number>(unit.offenses[1].att).set(7);
+  // table::add_unit(model_rules, book, "Marshal");
+  table::add_unit(model_rules, book, "State Infantry");
 }
